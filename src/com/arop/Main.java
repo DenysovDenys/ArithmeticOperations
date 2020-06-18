f9fd214cd1c3322b26a89b00594aa6cfa5a138af
Denysov Denys <denysovdenyswork@gmail.com>
2020-06-18 23:25:45 +0300

    commit operations

---
+++ src/com/arop/Main.java
@@ -0,0 +1,15 @@
+package com.arop;
+
+public class Main {
+    public static void main(String[] args) {
+        byte b = 1;
+        short sh = 2;
+        long l = 3;
+        double d = 4.0;
+        float f = 0.5f;
+        System.out.println(b + sh);
+        System.out.println(d - f);
+        System.out.println(l * b);
+        System.out.println(l / sh);
+    }
+}
