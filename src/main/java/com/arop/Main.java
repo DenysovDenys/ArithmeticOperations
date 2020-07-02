dd98165e37a86bcf6ffa16d030bf446f749fffeb
Denysov Denys <denysovdenyswork@gmail.com>
2020-07-02 22:35:46 +0300

    Created class that get array in constructor

--- src/main/java/com/arop/Main.java
+++ src/main/java/com/arop/Main.java
@@ -1,5 +1,6 @@
 package com.arop;
 
+import com.arop.generics.HighestAndLowest;
 import com.arop.generics.MyList;
 
 public class Main {
@@ -13,16 +14,24 @@ public class Main {
         listOfIntegers.Add(746);
         listOfIntegers.Add(85);
         listOfIntegers.Add(15);
-
         System.out.println("Max of integers: " + listOfIntegers.GetMax());
         System.out.println("Min of integers: " + listOfIntegers.GetMin());
-
         MyList listOfStrings = new MyList();
         listOfStrings.Add("Hello!");
         listOfStrings.Add("How are you?");
         listOfStrings.Add("I'm fine, thanks.");
-
         System.out.println("Max of integers: " + listOfStrings.GetMax());
         System.out.println("Min of integers: " + listOfStrings.GetMin());
+
+        Integer[] num = {534, 254, 32, 75, 977, 60, 275, 876, 91, 100};
+        HighestAndLowest<Integer> numbers = new HighestAndLowest<>(num);
+        System.out.println("Highest of integers: " + numbers.getMax());
+        System.out.println("Lowest of integers: " + numbers.getMin());
+
+        Double[] doubles = {-10.46, 3.14, 674.37, 35.12, -5.3, 36.6};
+        HighestAndLowest<Double> doubles2 = new HighestAndLowest<>(doubles);
+        System.out.println("Highest of double: " + doubles2.getMax());
+        System.out.println("Lower of double: " + doubles2.getMin());
+
     }
 }
---
+++ src/main/java/com/arop/generics/HighestAndLowest.java
@@ -0,0 +1,20 @@
+package com.arop.generics;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class HighestAndLowest<T extends Comparable<T>> {
+    private T[] array;
+
+    public HighestAndLowest(T[] array) {
+        this.array = array;
+    }
+
+    public T getMax() {
+        return Arrays.stream(array).max(Comparator.naturalOrder()).get();
+    }
+
+    public T getMin() {
+        return Arrays.stream(array).min(Comparator.naturalOrder()).get();
+    }
+}
