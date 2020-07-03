51d40e1ab2ec8d7c860c8cb06815ef101de9d68f
Denysov Denys <denysovdenyswork@gmail.com>
2020-07-03 22:39:56 +0300

    Added spaces between "="

--- src/main/java/com/arop/geometry/vertex/Vertex2D.java
+++ src/main/java/com/arop/geometry/vertex/Vertex2D.java
@@ -19,6 +19,6 @@ public class Vertex2D implements Vertex {
 
     @Override
     public String toString() {
-        return "{x=" + x + ", y=" + y + "}";
+        return "{x = " + x + ", y = " + y + "}";
     }
 }
