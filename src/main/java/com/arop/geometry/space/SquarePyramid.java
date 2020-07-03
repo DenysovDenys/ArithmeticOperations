b73d85918b51fa80eaf902dc7b70cc8dc02503b1
Denysov Denys <denysovdenyswork@gmail.com>
2020-07-03 22:40:38 +0300

    Renamed variable pyramidWight to width in SquarePyramid class

--- src/main/java/com/arop/geometry/space/SquarePyramid.java
+++ src/main/java/com/arop/geometry/space/SquarePyramid.java
@@ -1,29 +1,36 @@
 package com.arop.geometry.space;
 
+import com.arop.geometry.interfaces.AreaMeasurable;
+import com.arop.geometry.interfaces.VolumeMeasurable;
 import com.arop.geometry.vertex.Vertex3D;
 
 import java.util.List;
 
-public class SquarePyramid extends SpaceShape {
+public class SquarePyramid extends SpaceShape implements AreaMeasurable, VolumeMeasurable {
 
-    private final double pyramidWidth;
+    private final double width;
     private final double height;
 
 
-    public SquarePyramid(Vertex3D coordinates, double pyramidWight, double height) {
+    public SquarePyramid(Vertex3D coordinates, double width, double height) {
         super(List.of(coordinates), "Square Pyramid");
-        this.pyramidWidth = pyramidWight;
+        this.width = width;
         this.height = height;
     }
 
     @Override
     public double getArea() {
-        return (4 * pyramidWidth) / 2 * ((pyramidWidth / (2 * Math.tan(Math.toRadians(180 / 4.0))))
-                + Math.sqrt(Math.pow(height, 2) + (Math.pow(pyramidWidth / 2 * Math.tan(Math.toRadians(180 / 4.0)), 2))));
+        return (4 * width) / 2 * ((width / (2 * Math.tan(Math.toRadians(180 / 4.0))))
+                + Math.sqrt(Math.pow(height, 2) + (Math.pow(width / 2 * Math.tan(Math.toRadians(180 / 4.0)), 2))));
     }
 
     @Override
     public double getVolume() {
-        return (height * 4 * Math.pow(pyramidWidth, 2)) / 12 * Math.tan(Math.toRadians(180 / 4.0));
+        return (height * 4 * Math.pow(width, 2)) / 12 * Math.tan(Math.toRadians(180 / 4.0));
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Area: " + this.getArea() + ", Volume: " + this.getVolume();
     }
 }
