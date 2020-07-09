f62a74c659fdc0759a70f3035c00cf2cb7fb21e7
Denysov Denys <denysovdenyswork@gmail.com>
2020-07-09 23:24:22 +0300

    Created string of numbers and get rid of duplicate

--- src/main/java/com/arop/Main.java
+++ src/main/java/com/arop/Main.java
@@ -1,33 +1,9 @@
 package com.arop;
 
-import com.arop.arrayOperations.Fruits;
-import com.arop.arrayOperations.SimilarElements;
-import com.arop.arrayOperations.SortTreeSet;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.arop.mapOperations.RepeatingElements;
 
 public class Main {
     public static void main(String[] args) {
-        SimilarElements similarElements = new SimilarElements(new int[]{3, 9, 11, 18, 20, 22});
-        similarElements.removeDivisibleNumbers(3);
-
-        List<Integer> secSimilarElements = new ArrayList<>();
-        secSimilarElements.add(3);
-        secSimilarElements.add(11);
-        secSimilarElements.add(18);
-        similarElements.checkSimilarElements(secSimilarElements);
-
-        String[] allFruits = {"Banana", "Orange", "Apple", "Pear", "Tangerines", "Strawberry"};
-        Fruits fruits = new Fruits(allFruits);
-        fruits.replaceFruit("Orange", "Grapefruit");
-
-        String[] allFruitsForError = {"Banana", "Grapefruit", "Apple", "Pear", "Tangerines", "Strawberry"};
-        Fruits fruitsForError = new Fruits(allFruitsForError);
-        fruitsForError.replaceFruit("Potato", "Tomato");
-
-        int[] numbersForTreeSet = {3, 22, 43, 18, 8, 11};
-        SortTreeSet treeSet = new SortTreeSet(numbersForTreeSet);
-        treeSet.sortElementsByDescend();
+        RepeatingElements.outputUnique();
     }
 }
