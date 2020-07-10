f7c8d7538553f88ed78b39e7157437b0dabf5760
Denysov Denys <denysovdenyswork@gmail.com>
2020-07-10 23:35:34 +0300

    Created classes for stream operations: method finding users, who are older than age; method finding users, who are younger than age from city; method average age of users, who are from city; method how many users are not from city; method sort by age

--- src/main/java/com/arop/Main.java
+++ src/main/java/com/arop/Main.java
@@ -1,48 +1,30 @@
 package com.arop;
 
-import com.arop.mapOperations.ComparatorLengthNameOfEmblem;
-import com.arop.mapOperations.ComparatorLengthTitle;
-import com.arop.mapOperations.Family;
-import com.arop.mapOperations.RepeatingElements;
+import com.arop.streamOperations.StreamFunctions;
+import com.arop.streamOperations.User;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        RepeatingElements.outputUnique();
+        List<User> users = Arrays.asList(
+                new User("Basil", 16, "Dnipro"),
+                new User("Peter", 23, "Dnipro"),
+                new User("Elena", 42, "Lutsk"),
+                new User("Elena", 92, "Chernihiv"),
+                new User("Sergii", 5, "Kiev"),
+                new User("Marina", 32, "Kiev"),
+                new User("Ivan Ivanovich", 69, "Lviv"));
 
-        Family borodachenko = new Family("Borodachenko", 3, "Dukes", "Honor");
-        Family derkach = new Family("Derkach", 2, "Barons", "Glaive");
-        Family uspenko = new Family("Uspenko", 10, "Counts", "Horde");
-        Family chinkichi = new Family("Chinkichi", 4, "Boyar", "Chick");
-
+        StreamFunctions.outputOlderUsers(users, 40);
         System.out.println();
-
-        Map<Integer, Family> mapOfFamily = new HashMap<>();
-        mapOfFamily.put(3, borodachenko);
-        mapOfFamily.put(4, derkach);
-        mapOfFamily.put(1, uspenko);
-        mapOfFamily.put(2, chinkichi);
-        System.out.println("Comparable, two elements: " + borodachenko.compareTo(derkach));
-        ComparatorLengthNameOfEmblem comparatorLengthNameOfEmblem = new ComparatorLengthNameOfEmblem();
-        System.out.println("Comparator length name of emblem: " + comparatorLengthNameOfEmblem.compare(uspenko, chinkichi));
-        ComparatorLengthTitle comparatorLengthTitle = new ComparatorLengthTitle();
-        System.out.println("Comparator length title: " + comparatorLengthTitle.compare(derkach, uspenko));
-
-        Map<Integer, Family> mapTree = new TreeMap<>(mapOfFamily);
-        System.out.print("Collection sorted in natural order: ");
-        for (Map.Entry<Integer, Family> entry : mapTree.entrySet()) {
-            Integer key = entry.getKey();
-            System.out.print(key);
-        }
-
+        StreamFunctions.outputYoungerUsersFromCity(users, 50, "Dnipro");
         System.out.println();
-
-        System.out.println("Collection sorted by value: ");
-        mapOfFamily.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue())
-                .forEach(System.out::println);
+        StreamFunctions.outputAverageAgeOfUsersFromCity(users, "Lviv");
+        System.out.println();
+        StreamFunctions.outputNumberUsersNotFromCity(users, "Kiev");
+        System.out.println();
+        StreamFunctions.outputSortByAge(users, 3);
     }
 }
---
+++ src/main/java/com/arop/streamOperations/StreamFunctions.java
@@ -0,0 +1,46 @@
+package com.arop.streamOperations;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFunctions {
+    public static void outputOlderUsers(List<User> users, int age) {
+        System.out.println("Users, who are older than " + age + ": ");
+        users.stream()
+                .filter(user -> user.getAge() > age)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+
+    public static void outputYoungerUsersFromCity(List<User> users, int age, String city) {
+        System.out.println("Users, who are younger than " + age + " and from " + city + ": ");
+        users.stream()
+                .filter(user -> user.getAge() < age && user.getCity().equals(city))
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+
+    public static void outputAverageAgeOfUsersFromCity(List<User> users, String city) {
+        System.out.println("Average age of users, who are from " + city + ": ");
+        System.out.println((users.stream()
+                .filter(user -> user.getCity().equals(city))
+                .mapToInt(User::getAge).sum()) / (int) users.stream()
+                .filter(user -> user.getCity().equals(city)).count());
+    }
+
+    public static void outputNumberUsersNotFromCity(List<User> users, String city) {
+        System.out.println("How many users are not from " + city + ": ");
+        System.out.println(users.stream()
+                .filter(user -> !user.getCity().equals("Kiev"))
+                .count());
+    }
+
+    public static void outputSortByAge(List<User> users, int countOfElements) {
+        System.out.println("Sorted by age, first " + countOfElements + " elements: ");
+        users.stream()
+                .sorted(Comparator.comparingInt(User::getAge))
+                .limit(countOfElements)
+                .forEach(System.out::println);
+    }
+}
